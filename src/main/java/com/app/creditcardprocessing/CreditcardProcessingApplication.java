1566d161c6b88f646c2632c186710b83604bbf15
Anirban Roy <aroyiem@gmail.com>
2021-01-19 16:35:57 +0000

    Initial commit  . created project stub  . configured h2 database connectivity

---
+++ src/main/java/com/app/creditcardprocessing/CreditcardProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.app.creditcardprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditcardProcessingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/creditcardprocessing/CreditcardProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.creditcardprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcardProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
